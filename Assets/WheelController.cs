9bc391310bb52e18c83070caf5eb8b750ce17357
gamesketches <sam.von.ehren@gmail.com>
2020-12-02 21:07:14 -0500

    First commit, got a 'wheel' rolling

---
+++ Assets/WheelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float maxSpeed;
+    float inertia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationAmount = Input.GetAxis("Horizontal") * Time.deltaTime;
+        if(rotationAmount > 0)
+        {
+            inertia = Mathf.Min(maxSpeed, inertia + rotationAmount);
+            Debug.Log("Speeding up " + inertia.ToString());
+        }
+        else
+        {
+            inertia = Mathf.Max(0, inertia + rotationAmount);
+            Debug.Log("Speeding Down " + inertia.ToString());
+        }
+        transform.Rotate(0, 0, inertia);
+    }
+}
