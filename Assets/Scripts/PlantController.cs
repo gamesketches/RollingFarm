a1a0c0ec726b5ed480db062802727453c7f17b63
gamesketches <sam.von.ehren@gmail.com>
2020-12-05 01:04:28 -0500

    Updating version number, added in a little more feedback on watering plants

---
+++ Assets/Scripts/PlantController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum PlantStages {Seed, Sprout, Flowered, Dead}
+public class PlantController : MonoBehaviour
+{
+    public int cyclesToSprout;
+    int waterContent;
+    PlantStages currentStage;
+    SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterContent = 2;
+        currentStage = PlantStages.Seed;
+        renderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            waterContent--;
+            Debug.Log("Water content: " + waterContent);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("watering the plant");
+            waterContent++;
+            UpdateColor();
+        }
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(TryToPick())
+            {
+                GameObject.Find("PlantCounter").GetComponent<Text>().text = "Plants Raised: 1";
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("exiting");
+        cyclesToSprout--;
+        if(waterContent <= 0)
+        {
+            Debug.Log("plant dead :(");
+            Die();
+        }
+        else if (cyclesToSprout == 0)
+        {
+            GrowToNextStage();
+        }
+            UpdateColor();
+    }
+
+    bool TryToPick()
+    {
+        if(currentStage == PlantStages.Flowered)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void GrowToNextStage()
+    {
+        switch(currentStage)
+        {
+            case PlantStages.Seed:
+                currentStage = PlantStages.Sprout;
+                cyclesToSprout = 2;
+                break;
+            case PlantStages.Sprout:
+                currentStage = PlantStages.Flowered;
+                cyclesToSprout = 4;
+                break;
+            case PlantStages.Flowered:
+                currentStage = PlantStages.Dead;
+                cyclesToSprout = 10;
+                break;
+            case PlantStages.Dead:
+                Destroy(gameObject);
+                break;
+        }
+    }
+
+    void UpdateColor()
+    {
+        switch(currentStage)
+        {
+            case PlantStages.Seed:
+                renderer.color = Color.black;
+                break;
+            case PlantStages.Sprout:
+                renderer.color = Color.yellow;
+                break;
+            case PlantStages.Flowered:
+                renderer.color = Color.green;
+                break;
+            case PlantStages.Dead:
+                renderer.color = Color.grey;
+                break;
+        }
+        Debug.Log(renderer.color);
+        float h, s, v;
+        Color.RGBToHSV(renderer.color, out h, out s, out v);
+        Debug.Log(v);
+        v = Mathf.Lerp(0f, 1f, waterContent / 5f);
+        Debug.Log(v);
+        renderer.color = Color.HSVToRGB(h,s,v);
+        Debug.Log(renderer.color);
+    }
+
+    void Die()
+    {
+        currentStage = PlantStages.Dead;
+        cyclesToSprout = 10;
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameObject waterPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(SprinkleWater());
+        }
+    }
+
+    IEnumerator SprinkleWater() {
+        GameObject water = Instantiate<GameObject>(waterPrefab);
+        water.transform.position = transform.position;
+        for(float t = 0; t < 0.5f; t += Time.deltaTime) {
+            water.transform.position -= new Vector3(0, 0.01f, 0);
+            yield return null;
+        }
+        Destroy(water);
+    }
+
+}
