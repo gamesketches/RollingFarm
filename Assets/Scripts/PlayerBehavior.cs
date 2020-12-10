36f9371cf02b831933b1b394b35091f923dd005f
gamesketches <sam.von.ehren@gmail.com>
2020-12-09 20:46:24 -0500

    Wheel now visibly moves, player moves with it

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public GameObject waterPrefab;
+    public Transform wheel;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class PlayerBehavior : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)) {
             StartCoroutine(SprinkleWater());
         }
+        transform.Translate(wheel.position.x - transform.position.x, 0, 0);
     }
 
     IEnumerator SprinkleWater() {
--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class WheelController : MonoBehaviour
 {
     public float rotationSpeed;
-    public float maxSpeed;
+    public float maxRotationSpeed;
+    public float movement;
     float inertia;
     public float friction;
     // Start is called before the first frame update
@@ -17,16 +18,20 @@ public class WheelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float rotationAmount = Input.GetAxis("Horizontal") * Time.deltaTime;
-        if(rotationAmount > 0)
+        float rotationAmount = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        if(Mathf.Abs(rotationAmount) > 0)
         {
-            inertia = Mathf.Min(maxSpeed, inertia + rotationAmount);
+            inertia = Mathf.Min(maxRotationSpeed, inertia + rotationAmount);
         }
         else
         {
             inertia = Mathf.Max(0, inertia + rotationAmount);
         }
-        transform.Rotate(0, 0, inertia);
+        transform.Rotate(0, 0, -inertia);
+        Vector3 nextWheelPosition = new Vector3(transform.position.x + (inertia * movement), transform.position.y, 0);
+        transform.position = nextWheelPosition;
+        nextWheelPosition.z = Camera.main.transform.position.z;
+        Camera.main.transform.position = nextWheelPosition;
         if (inertia > 0) inertia -= friction;
     }
 }
