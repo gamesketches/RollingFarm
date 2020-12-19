a8879770912079cd61eaba0a0270b5607b0dd416
gamesketches <sam.von.ehren@gmail.com>
2020-12-19 17:32:23 -0500

    Added dynamic cursors for menus as well as a little recursion to how they navigate

---
+++ Assets/CursorController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorController : MonoBehaviour
+{
+    public GameObject[] options;
+    int cursorPosition;
+    public delegate void OnSelection();
+    public OnSelection menuSelection;
+    public bool activeCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorPosition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeCursor && Input.GetKeyDown(KeyCode.Space)) MakeSelection();
+        if(activeCursor) {
+            UpdatePosition();
+        }
+    
+    }
+
+    void UpdatePosition(){
+        if(Input.GetKeyDown(KeyCode.UpArrow)) {
+            cursorPosition--;
+            if(cursorPosition < 0) cursorPosition = options.Length - 1;
+            Debug.Log(cursorPosition);
+        } else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            cursorPosition++;
+            if(cursorPosition >= options.Length) cursorPosition = 0;
+        }
+        Vector3 newPosition = transform.position;
+        newPosition.y = options[cursorPosition].transform.position.y;
+        transform.position = newPosition;
+    }
+
+    void MakeSelection() {
+        activeCursor = false;
+        menuSelection();
+    }
+
+    public void TurnOnCursor(GameObject[] newOptions) {
+        options = newOptions;
+        activeCursor = true;
+        cursorPosition = 0;
+    }
+}
---
+++ Assets/ShopController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour
+{
+    GameObject categorySelector;
+    GameObject itemSelector;
+    GameObject moneyCounter;
+    TextBoxController itemCommentary;
+    bool categorySelected;
+    public string[] seedsForSale;
+    public string[] equipmentForSale;
+    public GameObject itemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seedsForSale = new string[] {"tom seed", "dude seed"};
+        categorySelector = transform.GetChild(0).gameObject;
+        categorySelector.GetComponentInChildren<CursorController>().menuSelection += LoadSeeds;
+        itemSelector = transform.GetChild(1).gameObject;
+        itemCommentary = transform.GetComponentInChildren<TextBoxController>();
+        moneyCounter = transform.GetChild(3).gameObject;
+        moneyCounter.GetComponentInChildren<Text>().text = "1000G";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadSeeds() {
+        List<GameObject> itemsForSale = new List<GameObject>();
+        for(int i = 0; i < seedsForSale.Length; i++) {
+           GameObject itemForSale = Instantiate<GameObject>(itemPrefab, itemSelector.transform);
+           itemForSale.GetComponent<RectTransform>().anchoredPosition = new Vector2(10 + (210*i), 10);
+            itemsForSale.Add(itemForSale);
+        }
+        itemSelector.GetComponentInChildren<CursorController>().TurnOnCursor(itemsForSale.ToArray());//.options = itemsForSale.ToArray();
+    }
+}
